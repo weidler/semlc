22c34530d9b11580537e23c31a5522d8a03f7ed0
Julian Lehnen <julianlehnen@gmail.com>
2019-04-26 14:44:55 +0200

    did guassian stuff better

--- util/gaussian_tensor.py
+++ util/gaussian_tensor.py
@@ -1,8 +1,16 @@
 import torch
+from scipy import signal
 
-def create_distributed_tensor(num_filters, num_samples=1000000, mean=1.0, std=0.0):
-    normal = torch.distributions.Normal(torch.tensor([mean]), torch.tensor([std]))
+
+def create_distributed_tensor_old(num_filters, num_samples=1000000, mean=1.0, std=0.5):
+    normal = torch.distributions.Normal(torch.tensor([mean], dtype=torch.float32), torch.tensor([std], dtype=torch.float32))
     samples = normal.sample((num_samples,))
     hist = torch.histc(samples, bins=num_filters)/(num_samples/float(num_filters))
     distr_tensor = hist.unsqueeze(0).unsqueeze(0)
+    return distr_tensor
+
+
+def create_distributed_tensor(num_filters, std):
+    window = signal.gaussian(num_filters, std=std)
+    distr_tensor = torch.Tensor([[window]])
     return distr_tensor
--- util/test_gaussian_tensor.py
+++ util/test_gaussian_tensor.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import gaussian_tensor as gt
 
 
-test = gt.create_distributed_tensor(32, mean=4.0, std=0.5)
+test = gt.create_distributed_tensor(32, 7)
 print(test)
 plt.plot(test.squeeze().numpy())
 plt.show()
