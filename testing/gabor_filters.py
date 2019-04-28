f6443ad4860a1d864ca228205a1fd655d3ffa3ce
Quinotn <q.denman@student.maastrichtuniversity.nl>
2019-04-28 16:06:22 +0200

    Approximation algorithm for finding the optimum filter ordering based on mse

--- testing/gabor_filters.py
+++ testing/gabor_filters.py
@@ -6,7 +6,8 @@ from skimage.filters import gabor_kernel
 import matplotlib.pyplot as plt
 from skimage.transform import resize
 
-from util.filter_ordering import order_dot_prod
+from util.filter_ordering import two_opt
+from util.filter_ordering import greedy
 
 filters = []
 
@@ -14,12 +15,15 @@ for orientation in range(0, 180, 180//12):
     filters.append(gabor_kernel(0.2, math.radians(orientation)).real)
 
 fig, axs = plt.subplots(3, 4)
+fig2, axs2 = plt.subplots(3, 4)
 
 shuffle(filters)
 max_size = max([f.shape[0] for f in filters])
 filters = [resize(f, (max_size, max_size)) for f in filters]
+filters2 = filters
 
-filters = order_dot_prod(filters)
+filters = two_opt(filters)
+filters2 = greedy(filters2)
 
 f = 0
 for row in range(3):
--- util/filter_ordering.py
+++ util/filter_ordering.py
@@ -1,8 +1,9 @@
 import random
 
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
 
-mse = lambda A, B: (numpy.square(A - B)).mean()
+mse = lambda A, B: (np.square(A - B)).mean()
 
 
 def greedy(filters):
@@ -14,3 +15,26 @@ def greedy(filters):
             filters.pop(min([(i, mse(f, focussed_filter)) for i, f in enumerate(filters)], key=lambda x: x[1])[0]))
 
     return ordered
+
+def two_opt(filters, improvement_threshold=0.001):  # 2-opt Algorithm adapted from https://en.wikipedia.org/wiki/2-opt
+    n_filters = len(filters)
+    path_distance = lambda r, c: np.sum([mse(c[r[p]], c[r[p - 1]]) for p in range(len(r))])
+    # Reverse the order of all elements from element i to element k in array r.
+    two_opt_swap = lambda r, i, k: np.concatenate((r[0:i], r[k:-len(r) + i - 1:-1], r[k + 1:len(r)]))
+    route = np.arange(n_filters)
+    print(route)
+    improvement_factor = 1
+    best_distance = path_distance(route, filters)  # Calculate the distance of the initial path.
+    while improvement_factor > improvement_threshold:  # ROute still improving?
+        distance_to_beat = best_distance
+        for swap_first in range(1, len(route) - 2):  # From each filter except the first and last,
+            for swap_last in range(swap_first + 1, len(route)):  # to each of the filters following,
+                new_route = two_opt_swap(route, swap_first, swap_last)  # try reversing the order of these filters
+                new_distance = path_distance(new_route,
+                                             filters)  # check the total distance with this modification.
+                print(new_distance)
+                if new_distance < best_distance:  # If the path distance is an improvement,
+                    route = new_route  # make this the accepted best route
+                    best_distance = new_distance  # and update the distance corresponding to this route.
+        improvement_factor = 1 - best_distance / distance_to_beat  # Calculate how much the route has improved.
+    return [filters[i] for i in route]  # When the route is no longer improving substantially, stop searching and return the route.
