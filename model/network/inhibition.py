18f473d24c48b320f18b16b0e9841eaa9480a879
Julian Lehnen <julianlehnen@gmail.com>
2019-04-28 14:23:24 +0200

    added abstract class for modules and visualization for filters

--- model/network/inhibition.py
+++ model/network/inhibition.py
@@ -3,18 +3,25 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from model.layer import Inhibition
+from model.inhibition_module import InhibitionModule
 
 
-class InhibitionClassificationCNN(nn.Module):
+class InhibitionClassificationCNN(nn.Module, InhibitionModule):
 
     def __init__(self):
         super().__init__()
 
+        self.layers = []
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.inhib1 = Inhibition(5)
         self.pool1 = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.inhib2 = Inhibition(5)
+
+        self.layers.append(self.conv1)
+        self.layers.append(self.inhib1)
+        self.layers.append(self.conv2)
+        self.layers.append(self.inhib2)
 
         self.classifying = nn.Sequential(
             nn.Linear(16 * 5 * 5, 120),
@@ -35,6 +42,9 @@ class InhibitionClassificationCNN(nn.Module):
         x = self.classifying(x)
 
         return x
+
+    def get_layers_for_visualization(self):
+        return self.layers
 
 
 if __name__ == "__main__":
