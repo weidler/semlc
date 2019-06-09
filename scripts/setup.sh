beaca1e7a0ac1723eb6824e1306891cccef922d7
Julian Lehnen <julianlehnen@gmail.com>
2019-06-09 15:28:46 +0200

    use python3.6

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # create virtualenv (torch installation fails otherwise)
-virtualenv .brains
+virtualenv --python=/usr/bin/python3.6 .brains
 source .brains/bin/activate
 
 # use cuda
 module load cuda
 
 # install pytorch for cuda
-pip install https://download.pytorch.org/whl/cu100/torch-1.1.0-cp27-cp27mu-linux_x86_64.whl
-pip install https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp27-cp27mu-linux_x86_64.whl
+#torch
+pip3 install https://download.pytorch.org/whl/cu100/torch-1.1.0-cp36-cp36m-linux_x86_64.whl
+
+#torchvision
+pip3 install https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp36-cp36m-linux_x86_64.whl
