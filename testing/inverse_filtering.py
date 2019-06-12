85a12c9846cd5000cbae4f60eaa9d84cd88cb174
weidler <web@tonioweidler.de>
2019-06-12 12:10:16 +0200

    Tested Inverse Filtering in pytorch; Implemented complex tensor division for this;

--- testing/inverse_filtering.py
+++ testing/inverse_filtering.py
@@ -4,6 +4,7 @@ Test equivalence of de-convolution and inversion of Toeplitz matrix.
 For stability reasons it is important to keep absolute values of lateral
 connection small. Below I multiply the Ricker kernel by 0.12 to achieve this.
 """
+import torch
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,6 +12,7 @@ from scipy.linalg import toeplitz, inv
 from scipy.signal import ricker, gaussian
 from scipy.fftpack import fft, ifft
 # %%
+from util.complex import div_complex
 
 n_points = 101
 w_ricker = 7
@@ -34,7 +36,7 @@ n_steps = 500
 a_1 = np.zeros(n_points)
 
 for i in range(n_steps):
-    a_1 = 0.95 * a_1 + 0.05 * (a_1 + z + np.dot(W, a_1))
+    a_1 += dt * (-a_1 + z + np.dot(W, a_1))
 
 """
 Scenario 2 (matrix multiplication):
@@ -54,9 +56,18 @@ implement by setting da/dt to 0 and solving for a:
 a = deconv((delta - k),z) 
 
 """
-delta = np.zeros(101)
+
+
+delta = torch.zeros(101).type(dtype=torch.double)
+z = torch.from_numpy(z).type(dtype=torch.double)
+k = torch.from_numpy(k).type(dtype=torch.double)
 delta[0] = 1
-a_3 = ifft(np.divide(fft(z), fft(delta-k)))
+fourier_z = torch.rfft(z, 1, onesided=False)
+fourier_convoluted = div_complex(fourier_z, torch.rfft(delta-k, 1, onesided=False))
+# fourier_convoluted = fourier_convoluted.where(~torch.isnan(fourier_convoluted), fourier_z)
+a_3 = torch.irfft(fourier_convoluted, 1, onesided=False)
+print(a_3)
+a_3 = a_3.numpy()
 
 # plotting
 plt.plot(a_1, label = 'recurrent')
---
+++ util/complex.py
@@ -0,0 +1,64 @@
+import time
+
+import torch
+
+
+def div_complex_iterative(a: torch.Tensor, b: torch.Tensor):
+    """Iterative division of complex tensors. Implemented to see if my approach works."""
+    assert a.shape[0] == b.shape[0]
+    assert a.shape[1] == 2 and b.shape[1] == 2
+
+    result = torch.zeros(a.shape)
+    for i in range(a.shape[0]):
+        numerator = a[i]
+        denominator = b[i]
+
+        # multiply by complex conjugate to make denominator real
+        denominator_real = denominator[0]**2 + (-denominator[1])**2
+        numerator_real = (numerator[0] * denominator[0]) - (numerator[1] * (-denominator[1]))
+        numerator_complex = (numerator[0] * (-denominator[1])) + (numerator[1] * denominator[0])
+
+        result[i][0] = numerator_real/denominator_real
+        result[i][1] = numerator_complex/denominator_real
+
+    return result
+
+
+def div_complex_vectorized(numerator: torch.Tensor, denominator: torch.Tensor):
+    """Vectorized division of complex tensors."""
+    assert numerator.shape[0] == denominator.shape[0]
+    assert numerator.shape[1] == 2 and denominator.shape[1] == 2
+
+    result = torch.zeros(numerator.shape)
+    denominator_real = denominator[:, 0] ** 2 + (-denominator[:, 1]) ** 2
+    numerator_real = (numerator[:, 0] * denominator[:, 0]) - (numerator[:, 1] * (-denominator[:, 1]))
+    numerator_complex = (numerator[:, 0] * (-denominator[:, 1])) + (numerator[:, 1] * denominator[:, 0])
+
+    result[:, 0] = numerator_real / denominator_real
+    result[:, 1] = numerator_complex / denominator_real
+
+    return result
+
+
+def div_complex(numerator: torch.Tensor, denominator: torch.Tensor):
+    """Optimized vectorized complex tensor division."""
+    assert numerator.shape[0] == denominator.shape[0]
+    assert numerator.shape[1] == 2 and denominator.shape[1] == 2
+
+    denominator_real = denominator[:, 0] ** 2 + (-denominator[:, 1]) ** 2
+    numerator_real = (numerator[:, 0] * denominator[:, 0]) - (numerator[:, 1] * (-denominator[:, 1]))
+    numerator_complex = (numerator[:, 0] * (-denominator[:, 1])) + (numerator[:, 1] * denominator[:, 0])
+
+    return torch.stack((numerator_real/denominator_real, numerator_complex/denominator_real), dim=1)
+
+
+if __name__ == "__main__":
+    # benchmarking
+    a = torch.randn((100, 2))
+    b = torch.randn((100, 2))
+
+    for method in [div_complex_iterative, div_complex_vectorized, div_complex]:
+        start = time.time()
+        for i in range(1000):
+            method(a, b)
+        print(f"{method.__name__}: {round(time.time() - start, 2)}s")
