90838e2780f0fd858e9ee9e4fc76051d2f152605
Julian Lehnen <julianlehnen@gmail.com>
2019-06-17 14:50:13 +0200

    adapted parameters

--- model/network/alexnet.py
+++ model/network/alexnet.py
@@ -116,23 +116,20 @@ class AlexNet(nn.Module):
         torch.nn.init.ones_(conv5.bias)
         self.features.add_module("conv_5", conv5)
         self.features.add_module("relu_5", nn.ReLU(inplace=True))
-        self.features.add_module("maxpool_3", nn.MaxPool2d(kernel_size=3, stride=2))
 
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
         self.classifier = nn.Sequential(
             nn.Dropout(),
-            nn.Linear(256 * 1 * 1, 4096),
+            nn.Linear(256 * 1 * 1, 128),
             nn.ReLU(inplace=True),
             nn.Dropout(),
-            nn.Linear(4096, 4096),
+            nn.Linear(128, 128),
             nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
+            nn.Linear(128, num_classes)
         )
 
     def forward(self, x):
         x = self.features(x)
-        x = self.avgpool(x)
-        x = x.view(x.size(0), 256 * 6 * 6)
+        x = x.view(x.size(0), 256 * 1 * 1)
         x = self.classifier(x)
 
         return x
@@ -143,3 +140,4 @@ if __name__ == '__main__':
     alex = AlexNet()
     #print(net.features)
     print(alex.features)
+    print(sum(p.numel() for p in alex.parameters()))
