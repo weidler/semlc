3a5d996987c149719b996c0269cdc70cd1261a37
Julian Lehnen <julianlehnen@gmail.com>
2020-02-05 19:39:29 +0100

    ordering visualisation

---
+++ visualisation/helper.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import torch
+
+from model.network.VGG import vgg19, vgg19_inhib
+from model.network.alexnet_cifar import SingleShotInhibitionNetwork, BaselineCMap, Baseline, ConvergedInhibitionNetwork, \
+    ParametricInhibitionNetwork
+
+keychain = "../../../experiments/all/keychain.txt"
+path = "../../../experiments/all/"
+
+df = pd.read_csv(keychain, sep="\t", names=['id', 'group', 'model', 'datetime'])
+
+
+def get_net(strategy: str):
+    # extend this for future experiments
+    all_nets = {
+        # baselines
+        'baseline': Baseline(),
+        'cmap': BaselineCMap(),
+
+        # ssi
+        'ss': SingleShotInhibitionNetwork([63], 8, 0.2, freeze=False),
+        'ss_freeze': SingleShotInhibitionNetwork([27], 3, 0.1, freeze=True),
+        'ss_freeze_zeros': SingleShotInhibitionNetwork([27], 3, 0.1, freeze=True, pad="zeros"),
+        'ss_freeze_self': SingleShotInhibitionNetwork([27], 3, 0.1, freeze=True, self_connection=True),
+        'ss_zeros': SingleShotInhibitionNetwork([63], 8, 0.2, freeze=False, pad="zeros"),
+        'ss_self': SingleShotInhibitionNetwork([63], 3, 0.1, freeze=False, self_connection=True),
+
+        # converged
+        'converged': ConvergedInhibitionNetwork([27], 3, 0.1, freeze=False),
+        'converged_freeze': ConvergedInhibitionNetwork([45], 3, 0.2, freeze=True),
+        'converged_zeros': ConvergedInhibitionNetwork([27], 3, 0.1, freeze=False, pad="zeros"),
+        'converged_freeze_zeros': ConvergedInhibitionNetwork([45], 3, 0.2, freeze=True, pad="zeros"),
+        'converged_self': ConvergedInhibitionNetwork([27], 3, 0.1, freeze=False, self_connection=True),
+        'converged_freeze_self': ConvergedInhibitionNetwork([45], 3, 0.2, freeze=True, self_connection=True),
+        'converged_cov_12': ConvergedInhibitionNetwork([27, 27], [3, 3], [0.1, 0.1], freeze=False, coverage=2),
+        'converged_cov_123': ConvergedInhibitionNetwork([27, 27, 27], [3, 3, 3], [0.1, 0.1, 0.1], freeze=False,
+                                                        coverage=3),
+        'converged_full': ConvergedInhibitionNetwork([27, 27, 27, 27], [3, 3, 3, 3], [0.1, 0.1, 0.1, 0.1], freeze=False,
+                                                     coverage=4),
+        'converged_full_best': ConvergedInhibitionNetwork([27, 63, 45, 31], [3, 10, 3, 10], [0.12, 0.1, 0.14, 0.12],
+                                                          freeze=False, coverage=4),
+
+        # parametric
+        'parametric': ParametricInhibitionNetwork([45], 3, 0.2),
+        'parametric_zeros': ParametricInhibitionNetwork([45], 3, 0.2, pad="zeros"),
+        'parametric_self': ParametricInhibitionNetwork([45], 3, 0.2, self_connection=True),
+        'parametric_12': ParametricInhibitionNetwork([63, 63], [3, 3], [0.2, 0.2], coverage=2),
+        'parametric_123': ParametricInhibitionNetwork([63, 63, 63], [3, 3, 3], [0.2, 0.2, 0.2], coverage=3),
+
+        # vgg
+        'vgg19': vgg19(),
+        'vgg19_inhib': vgg19_inhib()
+    }
+
+    return all_nets[strategy]
+
+
+def get_all_model_paths(strategy: str):
+    files = df[df['group'].str.match(rf'{strategy}_\d\d?')]['id']
+    return files
+
+
+def get_one_model(strategy: str, index=0):
+    model_path = get_all_model_paths(strategy).iloc[index]
+    filename = f"{path}{model_path}_best.model"
+    model = get_net(strategy)
+    model.load_state_dict(torch.load(filename, map_location=lambda storage, loc: storage))
+    return model
---
+++ visualisation/ordering.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+from matplotlib.axes import Axes
+from tqdm import tqdm
+
+from visualisation.helper import get_one_model
+from visualisation.plot_ordering import create_plot
+
+# 30 for parametric else 60
+num_nets = 30
+num_layer = 0
+
+# strategies = ['baseline', 'cmap', 'ss_freeze', 'ss', 'converged_freeze', 'converged', 'parametric']
+# strategies = ['converged' for _ in range(num_nets)]
+strategies = ['baseline', 'cmap', 'converged', 'converged_freeze', 'parametric']
+# strategies = ['converged_freeze', 'converged', 'parametric']
+# names = ["Baseline AlexNet", "Baseline AlexCMap", "Single Shot Frozen", "Single Shot Adaptive", "Converged Frozen", "Converged Adaptive", "Converged Parametric"]
+names = ["Baseline", "LRN", "CLC Adaptive", "CLC Frozen", "CLC Parametric"]
+# names = ["Converged Frozen", "Converged Adaptive", "Converged Parametric"]
+# names = ["Conv. Adapt." for _ in range(num_nets)]
+models = []
+
+indices = {
+    'parametric': 12,
+    'converged': 28,
+    'converged_freeze': 2,
+    'baseline': 0,
+    'cmap': 0
+}
+
+for i, strategy in enumerate(strategies):
+    model = get_one_model(strategy, index=indices[strategy])
+    # model = get_one_model(strategy, index=i)
+    models.append(model)
+
+print(models[0].features[num_layer])
+
+bc, ssi_c, conv_c = plt.get_cmap("Set1").colors[:3]
+
+# use for comparison of 5
+colors = [bc, bc, conv_c, conv_c, conv_c]
+
+# use for
+# colors = [conv_c for _ in range(num_nets)]
+
+r, c = 1, 5 #  get_dim_for_plot(len(names))
+print(r, c)
+gs = gridspec.GridSpec(r, c)
+fig, axs = plt.subplots(r, c)
+fig.set_size_inches(9, 2)
+
+# use for plotting all models
+rows = [i for i in range(r) for _ in range(c)]
+cols = [i for _ in range(r) for i in range(c)]
+
+# use for comparison of 5
+rows = [0, 0, 0, 0, 0]
+cols = [0, 1, 2, 3, 4]
+
+print(rows)
+print(cols)
+counter = 0
+
+
+for net in tqdm(models, disable=False):
+    filters = net.features[num_layer].weight.data.numpy()
+
+    plot_row = rows[counter]
+    plot_col = cols[counter]
+    ax: Axes = plt.subplot(gs[plot_row, plot_col])
+    if plot_col != 0:
+        ax.set_yticks([])
+    if plot_row != r - 1:
+        ax.set_xticks([])
+    create_plot(net, ax, cmap=colors[counter], num_layer=num_layer, point_size=2)
+    ax.set_title(names[counter])
+    counter += 1
+
+# ordering_{strategies[0]}.pdf
+fig.savefig(f'../documentation/figures/5ordering.pdf', format="pdf", bbox_inches='tight')
+plt.show()
