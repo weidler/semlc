36947c7ce95bd3b1a12198a7ffe3b9f9d9162a6e
weidler <web@tonioweidler.de>
2020-05-03 19:36:11 +0200

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --job-name=lcexp
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=admin@tonioweidler.de
+#SBATCH --time=24:00:00
+#SBATCH --nodes=3
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --partition=normal
+#SBATCH --constraint=gpu
+#SBATCH --hint=nomultithread
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+# load modules
+module load daint-gpu
+module load cray-python
+module load cray-nvidia-compute
+module load cudatoolkit/10.0.130_3.22-7.0.1.0_5.2__gdfb4ce5
+
+# load virtual environment
+source ${HOME}/lcvenv/bin/activate
+
+python3 -u model/network/train_efficient
