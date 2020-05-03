8631a8acd6d0cd6ae45f7793a194fa3f327aac5c
weidler <web@tonioweidler.de>
2020-05-03 19:37:24 +0200

    deploy change

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=lcexp
+#SBATCH --job-name=lcexp_en_base
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=admin@tonioweidler.de
 #SBATCH --time=24:00:00
@@ -23,4 +23,4 @@ module load cudatoolkit/10.0.130_3.22-7.0.1.0_5.2__gdfb4ce5
 # load virtual environment
 source ${HOME}/lcvenv/bin/activate
 
-python3 -u model/network/train_efficient
+python3 -u model/network/train_efficient_net.py data/imagenet/ -j=0 -a=efficientnet-b0 --opt_inh --gpu 0
