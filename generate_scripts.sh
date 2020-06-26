f3e91811f0f9beba3ec94fcc37f7766510106846
weidler <web@tonioweidler.de>
2020-06-26 09:03:39 +0200

    Fixed the script generator to produce valid calls to main.py; switched to CPU for HP optim;

--- generate_scripts.sh
+++ generate_scripts.sh
@@ -1,11 +1,11 @@
 #NUM=$1
-NUM=50
+NUM=48
 ITER=10
 
 for i in $(seq 0 $(($NUM-1)));
 do
-    head -n -1 deploy.sh > deploy_${i}.sh;
-    echo "python main.py -p ${i}" >> deploy_${i}.sh;
+    head -n -1 deploycpu.sh > deploy_${i}.sh;
+    echo "python CLC frozen main.py -p ${i}" >> deploy_${i}.sh;
 
     for k in $(seq 1 $ITER);
     do
