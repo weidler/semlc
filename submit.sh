b9b4abe553bf13444dcb0d01d852a512280f3f5a
Tonio Weidler <admin@tonioweidler.de>
2021-01-20 12:13:24 +0100

    Named parameter for submit script.

--- submit.sh
+++ submit.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
+while getopts ":p:" opt; do
+  case $opt in
+    p) SCRIPT="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
 if (($1 >= 0 && $1 <= 120)); then
   for ((i = 0 ; i < $1 ; i++)); do
-    sbatch --export=ALL,SCRIPTCOMMAND="$1" deploy.sh
+    sbatch --export=ALL,SCRIPTCOMMAND="$SCRIPT" deploy.sh
   done
 else
   echo "Dont do more than 120 (or less than 0)"
