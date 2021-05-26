87c07363284aeebad2d81ce261477f588e5eb492
Tonio Weidler <admin@tonioweidler.de>
2021-05-26 18:28:23 +0200

    Fixed COrnet Script

--- semlc/run_cornet_experiments.sh
+++ semlc/run_cornet_experiments.sh
@@ -5,7 +5,7 @@ if [ "$VARIANT" != "z" ] && [ "$VARIANT" != "s" ] ; then
   exit 1
 fi
 
-sh submit.sh -p "python3 run.py cornet-$VARIANT none -w 3,5 -e 250 --auto-group --data cifar10" -i $2
-sh submit.sh -p "python3 run.py cornet-$VARIANT parametric-semlc -w 3,5 -e 250 --auto-group --data cifar10" -i $2
-sh submit.sh -p "python3 run.py cornet-$VARIANT gaussian-semlc -w 3,5 -e 250 --auto-group --data cifar10" -i $2
-sh submit.sh -p "python3 run.py cornet-$VARIANT semlc -e 250 -w 3,5 --auto-group --data cifar10" -i $2
+sh submit.sh -p "python3 run.py cornet-$VARIANT none -w 3 5 -e 250 --auto-group --data cifar10" -i $2
+sh submit.sh -p "python3 run.py cornet-$VARIANT parametric-semlc -w 3 5 -e 250 --auto-group --data cifar10" -i $2
+sh submit.sh -p "python3 run.py cornet-$VARIANT gaussian-semlc -w 3 5 -e 250 --auto-group --data cifar10" -i $2
+sh submit.sh -p "python3 run.py cornet-$VARIANT semlc -e 250 -w 3 5 --auto-group --data cifar10" -i $2
